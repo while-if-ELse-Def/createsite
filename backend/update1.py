2509f2196e41b87c44bb6383af700c4279429525
while-if-ELse-Def <mailto:slashsasha03@gmail.com>
2024-02-10 22:43:22 +0500

    доступ к базе

--- backend/update1.py
+++ backend/update1.py
@@ -1,10 +1,44 @@
 import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+# Параметры соединения
+params = {
+    "user": "postgres",
+    "password": "postgres",
+    "host": "localhost",
+    "port": "5432"
+}
 
 # Создайте соединение
+conn = psycopg2.connect(**params)
+
+# Установите уровень изоляции для автокоммита
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+# Создайте курсор для выполнения SQL-запросов
+cur = conn.cursor()
+
+# Имя новой базы данных
+dbname = "site"
+
+# SQL-запрос для создания новой базы данных
+create_db_query = f"CREATE DATABASE {dbname};"
+
+
+# Выполните SQL-запрос
+
+cur.execute(create_db_query)
+# Закоммитить изменения
+conn.commit()
+
+# Закрыть курсор и соединение
+cur.close()
+conn.close()
+# Создайте соединение
 conn = psycopg2.connect(
-    dbname="your_database",
-    user="your_username",
-    password="your_password",
+    dbname=dbname,
+    user="C",
+    password="postgres",
     host="localhost",
     port="5432"
 )
@@ -14,10 +48,12 @@ cur = conn.cursor()
 
 # SQL-запросы для создания таблиц
 create_table_query1 = """
-CREATE TABLE table1 (
+CREATE TABLE certs (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL
+    price NUMERIC(10,2) NOT NULL,
+    description VARCHAR(1000) NULL,
+    
 );
 """
 
